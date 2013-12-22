13399e67f587aa0871cb8f3248aab5b43fa1fa94
ruuk <xbmc@2ndmind.net>
2013-12-22 13:42:07 -0800

    Most of the basics are here

---
+++ lib/bluraycomapi.py
@@ -0,0 +1,210 @@
+import re, requests, bs4, urllib # @UnresolvedImport
+
+class ResultItem:
+	_resultType = 'None'
+	_hr = '[COLOR FF606060]%s[/COLOR]' % ('_' * 200)
+	
+	def __init__(self,soupData):
+		self.title = ''
+		self.icon = ''
+		self.ID = ''
+		self.processSoupData(soupData)
+	
+	def __str__(self):
+		return '<%s=%s %s>' % (self._resultType,self.title,self.ID)
+	
+	def processSoupData(self,soupData):
+		pass
+	
+	def convertTable(self,soup,table,sep=' '):
+		if not table: return
+		text = '[CR]'
+		for td in table.findAll('tr'):
+			text += td.getText(separator=sep) + '[CR]'
+		tag = soup.new_tag('p')
+		tag.string = text
+		table.replaceWith(tag)
+		
+	def convertUL(self,soup,ul):
+		text = '[CR]'
+		for li in ul.findAll('li'):
+			text += li.getText() + '[CR]%s[CR]' % self._hr
+		tag = soup.new_tag('p')
+		tag.string = text
+		ul.replaceWith(tag)
+	
+class ReviewsResult(ResultItem):
+	_resultType = 'ReviewsResult'
+	
+	def __init__(self,soupData):
+		self.description = ''
+		self.rating = ''
+		self.ratingImage = ''
+		self.info = ''
+		self.url = ''
+		self.genre = ''
+		self.flagImage = ''
+		ResultItem.__init__(self, soupData)
+		
+	def processSoupData(self,soupData):
+		self.title = soupData.find('h3').getText().strip()
+		flagImg = soupData.find('img',{'src':lambda x: 'flag' in x})
+		if flagImg: self.flagImage = flagImg.get('src','')
+		images = soupData.findAll('img')
+		self.icon = images[0].get('src')
+		self.rating = images[-1].get('alt')
+		self.ratingImage = images[-1].get('src')
+		data = soupData.findAll('p',text=True)
+		try:
+			self.info = data[-3].text
+		except:
+			print 'INFO ERROR'
+		try:
+			self.description = data[-2].text
+		except:
+			print 'NO DESCRIPTION'
+		self.genre = data[-1].text
+		self.url = soupData.find('a').get('href')
+		self.ID = self.url.strip('/').rsplit('/')[-1]
+
+class ReviewsResultJSON(ReviewsResult):
+	def processSoupData(self,json):
+		self.title = json.get('title','')
+		self.url = json.get('url','')
+		self.icon = json.get('cover','')
+		self.flagImage = json.get('flag','')
+		rating = json.get('rating','')
+		self.ratingImage = 'http://www.blu-ray.com/images/rating/b%s.jpg' % (rating or '0')
+		self.info = json.get('year','') + ' | ' + json.get('reldate','')
+		try:
+			self.rating = 'Overall: %.1f of 5' % (int(rating)/2.0)
+		except:
+			pass
+		
+class Review(ReviewsResult):
+	_resultType = 'Review'
+	def __init__(self,soupData):
+		self.flagImage = ''
+		self.coverImage = ''
+		self.images = []
+		self.review = ''
+		self.overview = ''
+		self.specifications = ''
+		self.historyGraphs = []
+		self.otherEditions = []
+		self.similarTitles = []
+		ReviewsResult.__init__(self, soupData)
+		
+	def processSoupData(self,soupData):
+		flagImg = soupData.find('img',{'src':lambda x: 'flags' in x})
+		if flagImg: self.flagImage = flagImg.get('src','')
+		
+		for h3 in soupData.findAll('h3'):
+			for i in h3.findAll('img'):
+				tag = soupData.new_tag('p')
+				tag.string = i.get('alt','')
+				i.replaceWith(tag)
+			h3r = soupData.new_tag('p')
+			h3r.string = '[CR][COLOR FF0080D0][B]%s[/B][/COLOR][CR]' % h3.getText()
+			h3.replaceWith(h3r)
+			
+		for i in soupData.findAll('img',{'id':'reviewScreenShot'}):
+			src = i.get('src','')
+			p1080 = src.replace('.jpg','_1080p.jpg')
+			self.images.append((src,p1080))
+			idx = src.rsplit('.',1)[0].split('_',1)[-1]
+			tag = soupData.new_tag('p')
+			tag.string = '[CR][COLOR FFA00000]IMAGE %s[/COLOR][CR]' % idx
+			i.replaceWith(tag)
+		self.title = soupData.find('title').getText(strip=True)
+		
+		coverImg = soupData.find('img',{'id':'frontimage_overlay'})
+		if coverImg: self.coverImage = coverImg.get('src','')
+		
+		reviewSoup = soupData.find('div',{'id':'reviewItemContent'})
+		
+		if reviewSoup:
+			self.convertTable(soupData, reviewSoup.find('table'))
+			self.review = reviewSoup.getText(strip=True)
+			
+		if self.review.startswith('[CR]'): self.review = self.review[4:]
+		sections = soupData.findAll('div',{'data-role':'collapsible'})
+		
+		self.convertTable(soupData, sections[0].find('table'),sep=': ')
+		self.overview = sections[0].getText()
+		
+		self.convertUL(soupData,sections[1].find('ul'))
+		self.specifications = sections[1].getText()
+		if not len(sections) > 3: return
+		for i in sections[3].findAll('img'):
+			source = i.previous_sibling
+			source = source and ('[COLOR ' + source.string.rsplit('[COLOR ',1)[-1]) or '?'
+			source = re.sub('\[[^\]]+?\]','',source)
+			self.historyGraphs.append((source,i.get('src','')))
+		
+		if not len(sections) > 4: return
+		for li in sections[4].findAll('li'):
+			self.otherEditions.append((li.find('a').get('href'),li.find('img').get('src'),removeColorTags(li.getText())))
+		
+		if not len(sections) > 5: return
+		for li in sections[5].findAll('li'):
+			self.similarTitles.append((li.find('a').get('href'),li.find('img').get('src'),removeColorTags(li.getText())))
+		
+def removeColorTags(text):
+	return re.sub('\[/?COLOR[^\]]*?\]','',text)
+	
+class BlurayComAPI:
+	reviewsURL = 'http://m.blu-ray.com/movies/reviews.php'
+	releasesURL = 'http://m.blu-ray.com/movies'
+	searchURL = 'http://m.blu-ray.com/quicksearch/search.php?country=ALL&section=bluraymovies&keyword={0}'
+	pageARG = 'page=%s'
+	
+	def __init__(self):
+		pass
+	
+	def url2Soup(self,url):
+		req = requests.get(url)
+		return bs4.BeautifulSoup(req.text)
+	
+	def getCategories(self):
+		return [('Reviews','','reviews'),('Releases','','releases'),('Search','','search')]
+	
+	def getReleases(self):
+		items = []
+		soup = self.url2Soup(self.releasesURL)
+		for i in soup.findAll('li',{'data-role':lambda x: not x}):
+			items.append(ReviewsResult(i))
+		return items
+	
+	def getReviews(self,page=''):
+		if page: page = '?' + self.pageARG % page
+		soup = self.url2Soup(self.reviewsURL + page)
+		items = []
+		for i in soup.findAll('li',{'data-role':lambda x: not x}):
+			items.append(ReviewsResult(i))
+		return items
+	
+	def getReview(self,url):
+		req = requests.get(url)
+		
+		fixed = ''
+		for line in req.text.splitlines():
+			new = line.rstrip()
+			if new != line: new += ' '
+			line = new.lstrip()
+			if line != new: line = ' ' + line
+			fixed += line 
+		fixed = re.sub('<i>(?i)','[I]',fixed)
+		fixed = re.sub('</i>(?i)',' [/I]',fixed)
+		fixed = re.sub('<br[^>]*?>(?i)','[CR]',fixed)
+		soup = bs4.BeautifulSoup(fixed,from_encoding=req.encoding)
+		return Review(soup)
+	
+	def search(self,terms):
+		req = requests.get(self.searchURL.format(urllib.quote(terms)))
+		results = []
+		for i in req.json().get('items',[]):
+			results.append(ReviewsResultJSON(i))
+		return results
+		
+		
---
+++ lib/main.py
@@ -0,0 +1,215 @@
+import os, shutil, requests
+import xbmc, xbmcgui, xbmcaddon
+import bluraycomapi
+
+ADDON = xbmcaddon.Addon()
+
+CACHE_PATH = os.path.join(xbmc.translatePath(ADDON.getAddonInfo('profile')),'cache')
+
+if not os.path.exists(CACHE_PATH): os.makedirs(CACHE_PATH)
+
+def imageToCache(src,name):
+	response = requests.get(src, stream=True)
+	target = os.path.join(CACHE_PATH,name)
+	with open(target, 'wb') as out_file:
+		shutil.copyfileobj(response.raw, out_file)
+	del response
+	return target
+
+class BaseWindowDialog(xbmcgui.WindowXMLDialog):
+	def setProperty(self,key,value):
+		xbmcgui.Window(xbmcgui.getCurrentWindowDialogId()).setProperty(key,value)
+		xbmcgui.WindowXMLDialog.setProperty(self,key,value)
+	
+class BluRayCategories(xbmcgui.WindowXML):
+	def __init__(self,*args,**kwargs):
+		xbmcgui.WindowXML.__init__(self)
+	
+	def onInit(self):
+		self.catList = self.getControl(100)
+		self.showCategories()
+		
+	def showCategories(self):
+		items = []
+		for label, icon, ID in API.getCategories():
+			item = xbmcgui.ListItem(label=label,iconImage=icon)
+			item.setProperty('id',ID)
+			items.append(item)
+		self.catList.addItems(items)
+		self.setFocus(self.catList)
+		
+	def onClick(self,controlID):
+		if controlID == 100:
+			item = self.catList.getSelectedItem()
+			if not item: return
+			if item.getProperty('id') == 'reviews':
+				openReviewsWindow()
+			elif item.getProperty('id') == 'releases':
+				openReviewsWindow(mode='RELEASES')
+			elif item.getProperty('id') == 'search':
+				key = xbmc.Keyboard(heading='Enter Terms')
+				key.doModal()
+				if not key.isConfirmed(): return
+				terms = key.getText()
+				if not terms: return
+				del key
+				openReviewsWindow(search=terms)
+				
+class BluRayReviews(xbmcgui.WindowXMLDialog):
+	def __init__(self,*args,**kwargs):
+		self.search = kwargs.get('search')
+		self.mode = kwargs.get('mode')
+		xbmcgui.WindowXML.__init__(self)
+	
+	def onInit(self):
+		self.reviewList = self.getControl(101)
+		self.loading = self.getControl(149)
+		self.showReviews()
+	
+	def showReviews(self):
+		self.loading.setVisible(True)
+		try:
+			items = []
+			if self.search:
+				results = API.search(self.search)
+				if not results:
+					xbmcgui.Dialog().ok('No Results','Search query provided no results.')
+					return
+			elif self.mode == 'RELEASES':
+				results = API.getReleases()
+			else:
+				results = API.getReviews()
+				
+			for i in results:
+				item = xbmcgui.ListItem(label=i.title,iconImage=i.icon)
+				item.setProperty('id',i.ID)
+				item.setProperty('description',i.description)
+				item.setProperty('info',i.info)
+				item.setProperty('genre',i.genre)
+				item.setProperty('rating',i.rating)
+				item.setProperty('ratingImage',i.ratingImage)
+				item.setProperty('url',i.url)
+				item.setProperty('flag',i.flagImage)
+				items.append(item)
+			self.reviewList.addItems(items)
+			self.setFocus(self.reviewList)
+		finally:
+			self.loading.setVisible(False)
+		
+	def onClick(self,controlID):
+		if controlID == 101:
+			item = self.reviewList.getSelectedItem()
+			if not item: return
+			openWindow(BluRayReview, 'bluray-com-review.xml',url=item.getProperty('url'))
+	
+class BluRayReview(BaseWindowDialog):
+	def __init__(self,*args,**kwargs):
+		self.url = kwargs.get('url')
+		xbmcgui.WindowXML.__init__(self)
+		
+	def onInit(self):
+		self.imagesList = self.getControl(102)
+		self.altList = self.getControl(134)
+		self.reviewText = self.getControl(130)
+		self.infoText = self.getControl(132)
+		self.loading = self.getControl(149)
+		self.showReview()
+	
+	def reset(self,link):
+		self.url = link
+		self.imagesList.reset()
+		self.altList.reset()
+		self.reviewText.reset()
+		self.infoText.reset()
+		self.showReview()
+		
+	def showReview(self):
+		self.loading.setVisible(True)
+		try:
+			review = API.getReview(self.url)
+			self.setProperty('title', review.title)
+			self.setProperty('flag', review.flagImage)
+			self.setProperty('cover', review.coverImage)
+			self.reviewText.setText(review.review)
+			
+			self.infoText.setText(('[CR][B]%s[/B][CR]' % ('_' * 200)).join((review.overview,review.specifications)))
+			
+			items = []
+			for url,url_1080p in review.images:
+				item = xbmcgui.ListItem(iconImage=url)
+				item.setProperty('1080p',url_1080p)
+				items.append(item)
+			ct = 0
+			for source, url in review.historyGraphs:  # @UnusedVariable
+				url = imageToCache(url,'graph%s.png' % ct)
+				item = xbmcgui.ListItem(iconImage=url)
+				item.setProperty('1080p',url)
+				items.append(item)
+				ct += 1
+			self.imagesList.addItems(items)
+			
+			items = []
+			if review.otherEditions:
+				item = xbmcgui.ListItem(label='Other Editions')
+				item.setProperty('separator','1')
+				items.append(item)
+				
+			for link, image, text in review.otherEditions:
+				item = xbmcgui.ListItem(iconImage=image)
+				item.setProperty('link',link)
+				item.setProperty('text',text)
+				items.append(item)
+				
+			if review.similarTitles:
+				item = xbmcgui.ListItem(label='Similar')
+				item.setProperty('separator','1')
+				items.append(item)
+				
+			for link, image, text in review.similarTitles:
+				item = xbmcgui.ListItem(iconImage=image)
+				item.setProperty('link',link)
+				item.setProperty('text',text)
+				items.append(item)
+			self.altList.addItems(items)
+			self.setFocus(self.imagesList)
+		finally:
+			self.loading.setVisible(False)
+		
+	def onClick(self,controlID):
+		if controlID == 102:
+			item = self.imagesList.getSelectedItem()
+			if not item: return
+			openWindow(ImageViewer, 'bluray-com-image.xml',url=item.getProperty('1080p'))
+		elif controlID == 134:
+			item = self.altList.getSelectedItem()
+			if not item: return
+			link = item.getProperty('link')
+			if not link: return
+			self.reset(link)
+			
+
+class ImageViewer(xbmcgui.WindowXMLDialog):
+	def __init__(self,*args,**kwargs):
+		self.url = kwargs.get('url')
+		xbmcgui.WindowXML.__init__(self)
+		
+	def onInit(self):
+		self.getControl(150).setImage(self.url)
+		
+def setGlobalSkinProperty(key,value=''):
+	xbmcgui.Window(10000).setProperty('script.bluray.com-%s' % key,value)
+	
+def openReviewsWindow(search=None,mode=None):
+	setGlobalSkinProperty('reviews_open','1')
+	openWindow(BluRayReviews,'bluray-com-reviews.xml',search=search,mode=mode)
+	setGlobalSkinProperty('reviews_open','0')
+		
+		
+def openWindow(window_class,xml_file,**kwargs):
+	w = window_class(xml_file , xbmc.translatePath(ADDON.getAddonInfo('path')), 'Main',**kwargs)
+	w.doModal()
+	del w
+	
+if __name__ == '__main__':
+	API = bluraycomapi.BlurayComAPI()
+	openWindow(BluRayCategories,'bluray-com-categories.xml')
